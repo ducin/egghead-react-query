022af45a40d38a7da6fbd025d087027bc90af14a
Tomasz Ducin <tomasz@ducin.dev>
2024-05-12 01:54:36 +0200

    lesson-03

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import "./index.css";
 
 import { router } from "./router.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
 
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   </React.StrictMode>
 );
