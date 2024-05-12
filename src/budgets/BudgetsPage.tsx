6a12db8ad8f1b15aa6c1f5ee06b84366812e8932
Tomasz Ducin <tomasz@ducin.dev>
2024-05-12 22:19:38 +0200

    lesson-09

--- src/budgets/BudgetsPage.tsx
+++ src/budgets/BudgetsPage.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { BudgetTable } from "./BudgetTable";
 import { Benefit, Employee } from "../api/dto";
+import { useQuery } from "@tanstack/react-query";
+import { employeesQuery } from "../api/employeesQueries";
 
 function salariesCosts(employees: Employee[]) {
   const monthly = employees.reduce((total, e) => total + e.salary, 0);
@@ -17,8 +19,11 @@ function benefitCosts(benefits: Benefit[]) {
 interface BudgetsPageProps {}
 
 export const BudgetsPage: React.FC<BudgetsPageProps> = () => {
-  const salaries = salariesCosts([]);
+  const { data } = useQuery(employeesQuery);
+
+  const salaries = data && salariesCosts(data);
   // const salaries = { monthly: 0, yearly: 0 };
+
   const benefits = benefitCosts([]);
   // const benefits = { monthly: 0, yearly: 0 };
 
